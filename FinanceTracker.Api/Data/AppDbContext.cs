c92f51ab1ebf58d0603019b4a33bc3e340ceec5c
Eduardo Fantim <edugbf5@gmail.com>
2026-05-15 18:24:27 -0300

    fix: libraries missing

--- FinanceTracker.Api/Data/AppDbContext.cs
+++ FinanceTracker.Api/Data/AppDbContext.cs
@@ -1,3 +1,6 @@
+using FinanceTracker.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace FinanceTracker.Api.Data
 {
     public class AppDbContext : DbContext
--- FinanceTracker.Api/Program.cs
+++ FinanceTracker.Api/Program.cs
@@ -1,3 +1,6 @@
+using FinanceTracker.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.

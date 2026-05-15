30b8166e6e7df6874f8531d1c0d8d3a5827a3fb1
Eduardo Fantim <edugbf5@gmail.com>
2026-05-15 18:21:46 -0300

    build: sql server config

--- FinanceTracker.Api/Program.cs
+++ FinanceTracker.Api/Program.cs
@@ -3,6 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllers();
+
 
 var app = builder.Build();
 
@@ -13,10 +17,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.MapControllers();
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}

7478def8531b1246ed9a0ed3ce7a2caa9a39f1ba
Eduardo Fantim <edugbf5@gmail.com>
2026-05-15 18:14:39 -0300

    build: new transactions model

---
+++ FinanceTracker.Api/Models/Transaction.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinanceTracker.Api.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        [MaxLength(250)]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        public TransactionType Type { get; set; }
+
+            // Relacionamento com User
+        [Required]
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+
+            // Relacionamento com Category
+        [Required]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+    }
+}
